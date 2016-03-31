5d9d817c7448a6914e55105831ab7d2ac1dbd156
Gary Marx <gary@zenpayroll.com>
2016-03-31 14:18:09 -0700

    setup devise emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,5 @@ Rails.application.configure do
 
   # for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
 end
