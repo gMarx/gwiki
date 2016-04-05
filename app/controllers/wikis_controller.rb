425ef9e298f412d214f671969bf1d55179e0daf6
Gary Marx <gary@zenpayroll.com>
2016-04-05 11:56:44 -0700

    add methods to wikis controller

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -7,18 +7,34 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
   end
 
   def edit
-    # @wiki = Wiki.find(params[:id])
+    @wiki = Wiki.find(params[:id])
   end
 
   def create
   end
 
   def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.update_attributes( wiki_params )
+
+    if @wiki.save
+      flash[:notice] = 'Wiki was updated'
+      redirect_to @wiki
+    else
+      flash.now[:alert] = 'There was an error saving the wiki. Please try again.'
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :private)
   end
 end
