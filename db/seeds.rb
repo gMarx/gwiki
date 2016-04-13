52a3724bfab1a0143afdaf199c94b0c685b7bf08
Gary Marx <gary@zenpayroll.com>
2016-04-13 15:23:44 -0700

    seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
 # create 50 wikis
 50.times do
   Wiki.create!(
-    title: Faker::Lorem.sentence ,
+    title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph,
     user_id: 1
+  )
+end
+
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    confirmed_at: Faker::Time.between(DateTime.now - 4, DateTime.now),
+    role: 0,
+    password: Faker::Internet.password
   )
 end
 
