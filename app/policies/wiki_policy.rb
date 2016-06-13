6022bf156f72033d5b81f9404ff2334153a7d13a
Gary <gamarx@gmail.com>
2016-06-13 11:33:30 -0700

    ability to create private wikis

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,9 +1,20 @@
 class WikiPolicy < ApplicationPolicy
   def update?
-    record.present? && (record.user == user || record.public)
+    record.present? && (record.user == user || !record.private? )
+  end
+
+  def edit?
+    # allow all users to edit any wiki (they can see)
+    record.present?
+  end
+
+  def view?
+    # see public wikis & private ones created by self
+    record.present? && (record.user == user || record.user.role == :admin || record.private? == false)
   end
 
   def destroy?
-    record.present? && record.user == user
+    # only allow a user (or admin) to destroy their own wiki(s)
+    record.present? && (record.user == user || record.user.role == :admin)
   end
 end
